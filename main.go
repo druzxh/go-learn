4152825c2a941765d4d30c9cb7dd72feada67866
druzxh <badrudin.on@gmail.com>
2023-11-29 17:00:00 +0700

    init main

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	//init var
+	var name string
+
+	// print text
+	fmt.Println("Hello world")
+
+	// assign var
+	name = "Badru"
+
+	// print var
+	fmt.Println(name)
+
+	// variable
+	iniVariabel := "Isinya variable"
+	fmt.Println(iniVariabel)
+
+	// double variable
+	var (
+		firstName = "Jack"
+		lastName  = "Kie"
+	)
+
+	// Join var
+	fullName := firstName + lastName
+
+	fmt.Println(fullName)
+}
