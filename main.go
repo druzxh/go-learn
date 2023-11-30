2b4cd0820b44fc8e4b7e6fe8f07dba4248e6853a
druzxh <badrudin.on@gmail.com>
2023-11-30 08:32:43 +0700

    go data types

--- main.go
+++ main.go
@@ -29,4 +29,41 @@ func main() {
 	fullName := firstName + lastName
 
 	fmt.Println(fullName)
+
+	// number
+
+	fmt.Println("satu", 1)
+	fmt.Println("dua", 2)
+	fmt.Println("float", 2.5)
+
+	// boolean
+	fmt.Println("true", true)
+	fmt.Println("false", false)
+
+	// string
+	fmt.Println("Ini adalah string")
+
+	// func string
+	fmt.Println("panjang", len("Panjang string"))
+	fmt.Println("string ke 5"[1])
+
+	// constant
+	const dataName = "Data constant"
+
+	// multiple constant
+	const (
+		firstConst  = "Data first"
+		secondConst = "Data second"
+	)
+
+	// convert data
+
+	var value32 int = 32768
+	var value64 int64 = int64(value32)
+	var value16 int16 = int16(value32)
+
+	fmt.Println("val32", value32)
+	fmt.Println("val64", value64)
+	fmt.Println("val16", value16)
+
 }
